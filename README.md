b1f9969a4f59654d8d93140ecf795b95b864db6e
Disha Patel <dishatridhyatech@gmail.com>
2021-06-03 09:23:08 +0530

    covid tracker works fine

---
+++ README.md
@@ -0,0 +1,13 @@
+# Covid Tracker
+
+This Project is create in React Native with Redux to demonstrate Covid Data State wise for India with Active, Confirmed, Recovered, Deaths cases
+
+## Getting Started
+
+1) Clone the Project
+2) Install node modules with help of npm install 
+3) Run command react-native run-android
+
+# Note
+
+This Project is tested in samsung m30. 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,24 +1,82 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, StatusBar, View, SafeAreaView, StyleSheet } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux';
 import { getCovidData } from '../actions/covid.tracker.action';
+import { Picker } from '@react-native-picker/picker';
+import moment from 'moment'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
 
-    const [covidData, setCovidData] = useState(null);
     const [countryData, setCountryData] = useState([]);
+    const [state, setState] = useState('');
+    const [date,setDate] = useState('');
+    const [loading, setLoading] = useState(true);
 
-    const dispatch = useDispatch();
-    useEffect(async ()=>{
-       dispatch(getCovidData());
-    },[])
-
+    const [counts, setCounts] = useState({
+        active: 0,
+        confirmed: 0,
+        deaths: 0,
+        recovered: 0
+    })
 
     const data = useSelector(state => state.covidDataReducer.data);
-    console.log("data", data);
-   
-    
-    // console.log("key", Object.getOwnPropertyNames(data)); 
+    if (data) {
+        let keys = Object.keys(data);
+        let values = Object.values(data)
+        var allData = [];
+        keys.forEach((el, i) => {
+            allData.push({ state: el, data: values[i] })
+        });
+    }
+    const dispatch = useDispatch();
+    useEffect(async () => {
+        dispatch(getCovidData());
+        setCountryData(allData);
+        setLoading(false);
+        setDate(moment().format("DD MMM,YYYY HH:mm A"));
+    }, [])
+
+    const addCommas = (num) => {
+        input = num;
+        var n1, n2;
+        num = num + '' || '';
+        n1 = num.split('.');
+        n2 = n1[1] || null;
+        n1 = n1[0].replace(/(\d)(?=(\d\d)+\d$)/g, "$1,");
+        num = n2 ? n1 + '.' + n2 : n1;
+        return num;
+    }
+
+    const onStateSelect = (id) => {
+        let activeCount = 0;
+        let confirmedCount = 0;
+        let recoveredCount = 0;
+        let deathsCount = 0;
+
+        let state = countryData[id];
+        let keys = Object.keys(state.data.districtData);
+        let values = Object.values(state.data.districtData);
+        let allStateData = [];
+
+        keys.forEach((el, i) => {
+            allStateData.push({ city: el, data: values[i] })
+        });
+
+        allStateData.forEach(el => {
+            activeCount = activeCount + el.data.active;
+            confirmedCount = confirmedCount + el.data.confirmed;
+            recoveredCount = recoveredCount + el.data.recovered;
+            deathsCount = deathsCount + el.data.deceased;
+        });
+
+        setCounts({
+            active: addCommas(activeCount),
+            confirmed: addCommas(confirmedCount),
+            recovered: addCommas(recoveredCount),
+            deaths: addCommas(deathsCount)
+        })
+
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -26,140 +84,133 @@ const Dashboard = () => {
                 backgroundColor={"#4169E1"} />
 
             <View style={{ flex: 1 }}>
-                <View style={{ flex: 1, backgroundColor: '#4169E1' }}>
-                    <View style={{ marginTop: '30%', justifyContent: 'center' }}>
+                <View style={styles.header}>
+                    <View style={styles.headerContainer}>
                         <Text style={styles.title}>Covid Tracker</Text>
-                        <Text style={{ textAlign: 'center', fontSize: 24, color: '#fff', fontWeight: '700' }}>INDIA</Text>
+                        <Text style={[styles.subTitle,styles.f700]}>INDIA</Text>
                     </View>
 
-                    <View style={{ marginTop: '10%', justifyContent: 'center' }}>
-                        <Text style={{ textAlign: 'center', fontSize: 24, color: '#fff', fontWeight: '600' }}>State</Text>
+                    <View style={styles.pickerContainer}>
+                        <Text style={[styles.subTitle,styles.f600]}>State</Text>
+                        <Picker  
+                             iosHeader="Select one"
+                             mode="dropdown"
+                            selectedValue={state}
+                            onValueChange={(itemValue, itemIndex) => {
+                                setState(itemIndex);
+                                onStateSelect(itemIndex);
+                            }}>
+                            {(countryData && !loading) ? countryData.map((item, i) =>
+                                <Picker.Item  color='white' label={item.state} value={i} key={i}/>
+                            )
+                                : <Picker.Item  color='white'  label={"No Data"} value={''}  key={''} />
+                            }
+                        </Picker>
                     </View>
                 </View>
                 <View style={{ flex: 1 }}>
                     <View style={{ marginTop: '10%', marginLeft: 10 }}>
-                        <Text style={{ fontSize: 16, fontWeight: '600' }}>Case Update</Text>
-                        <Text style={{ fontSize: 14, fontWeight: '400', color: '#A9A9A9' }}>Newest update on</Text>
+                        <Text style={styles.font16,styles.f700}>Case Update</Text>
+                        <Text style={styles.date}>Newest update on {date}</Text>
                     </View>
 
-                    <View style={{
-                        marginTop: '5%', flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        paddingLeft: '5%',
-                        paddingRight: '10%'
-                    }}>
-
-                        <View style={{
-                            height: 100, backgroundColor: '#fff',
-                            elevation: 5,
-                            borderRadius: 5,
-                            borderColor: '#000',
-                            justifyContent: 'center'
-                            , marginRight: 20, width: '50%'
-                        }}>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                fontWeight: '600', color: '#ff073a', fontSize: 16
-                            }}>Confirmed</Text>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                fontWeight: '600', color: '#ff073a', fontSize: 14
-                            }}>2,00,000</Text>
+                    <View style={styles.casesContainer}>
+                        <View style={styles.casesSubContainer}>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorConfrirmed,styles.font16]}>Confirmed</Text>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorConfrirmed,styles.font14]}>{counts.confirmed}</Text>
                         </View>
 
-                        <View style={{
-                            height: 100, backgroundColor: '#fff',
-                            elevation: 5,
-                            borderRadius: 5,
-                            borderColor: '#000',
-                            justifyContent: 'center',
-                            width: '50%'
-                        }}>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                fontWeight: '600', color: '#007bff', fontSize: 16
-                            }}>Active</Text>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                fontWeight: '600', color: '#007bff', fontSize: 14
-                            }}>2,00,000</Text>
+                        <View style={styles.casesSubContainer}>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorActive,styles.font16]}>Active</Text>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorActive,styles.font14]}>{counts.active}</Text>
                         </View>
 
                     </View>
 
-                    <View style={{
-                        marginTop: '5%', flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        paddingLeft: '5%',
-                        paddingRight: '10%',
-                    }}>
-
-                        <View style={{
-                            height: 100, backgroundColor: '#fff',
-                            elevation: 5,
-                            borderRadius: 5,
-                            borderColor: '#000',
-                            justifyContent:'center'
-                            , marginRight: 20, width: '50%'
-                        }}>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                fontWeight: '600', color: '#28a745', fontSize: 14
-                            }}>Recovered</Text>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                fontWeight: '600', color: '#28a745', fontSize: 12
-                            }}>2,00,000</Text>
+                    <View style={styles.casesContainer}>
+                        <View style={styles.casesSubContainer}>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorRecovered,styles.font16]}>Recovered</Text>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorRecovered,styles.font14]}>{counts.recovered}</Text>
                         </View>
 
-                        <View style={{
-                            height: 100, backgroundColor: '#fff',
-                            elevation: 5,
-                            borderRadius: 5,
-                            borderColor: '#000',
-                            justifyContent:'center'
-                            , marginRight: 20, width: '50%'
-                        }}>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                marginTop: 10,
-                                fontWeight: '600', color: '#6c757d', fontSize: 16
-                            }}>Deaths</Text>
-
-                            <Text style={{
-                                textAlign: 'center',
-                                marginTop: 10,
-                                fontWeight: '600', color: '#6c757d', fontSize: 14
-                            }}>2,00,000</Text>
+                        <View style={styles.casesSubContainer}>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorDeath,styles.font16]}>Deaths</Text>
+                            <Text style={[styles.textCenter,styles.f600,styles.colorDeath,styles.font14]}>{counts.deaths}</Text>
                         </View>
 
                     </View>
                 </View>
             </View>
 
-        </SafeAreaView>
+        </SafeAreaView >
     )
 }
 
-export default Dashboard
 
 const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-    },title:{
-         textAlign: 'center', 
-         fontSize: 24, 
-         color: '#fff', 
-         fontWeight: '600'
+    },header:{
+        flex: 1, 
+        backgroundColor: '#4169E1',
+        borderBottomLeftRadius:50,
+        borderBottomRightRadius:50
+    }, title: {
+        textAlign: 'center',
+        fontSize: 24,
+        color: '#fff',
+        fontWeight: '600'
+    },headerContainer:{
+        marginTop: '30%',
+        justifyContent: 'center'
+    },subTitle:{
+        textAlign: 'center', 
+        fontSize: 24, 
+        color: '#fff',
+    },f700:{
+        fontWeight:'700'
+    },f600:{
+        fontWeight:'600'
+    },pickerContainer:{
+        marginTop: '10%',
+        justifyContent: 'center'
+    },font16:{
+        fontSize:16
+    },font14:{
+        fontSize:14
+    },date:{
+        fontSize:14,
+        fontWeight:'700',
+        color: '#A9A9A9'
+    },casesContainer:{
+        marginTop: '5%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: '5%',
+        paddingRight: '10%'
+    },casesSubContainer:{
+        height: 100,
+        backgroundColor: '#fff',
+        elevation: 5,
+        borderRadius: 5,
+        borderColor: '#000',
+        justifyContent: 'center',
+        borderRadius:10,
+        marginRight: 20,
+         width: '50%'
+    },colorActive:{
+        color:'#007bff',
+    },colorRecovered:{
+        color:'#28a745'
+    },colorDeath:{
+        color: '#6c757d'
+    },colorConfrirmed:{
+        color:'#ff073a'
+    },textCenter:{
+        textAlign:'center'
     }
-
-
 });
+
+
+export default Dashboard;
